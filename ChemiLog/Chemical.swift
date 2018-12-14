12398b8b167b75216e4af8461da0a587f8b57faf
Parker Swanson <parker@liquidspace.com>
2018-12-14 10:59:59 -0600

    chemical test

---
+++ ChemiLog/Chemical.swift
@@ -0,0 +1,20 @@
+//
+//  Chemical.swift
+//  ChemiLog
+//
+//  Created by Parker Swanson on 12/14/18.
+//  Copyright © 2018 District196.org. All rights reserved.
+//
+
+import Foundation
+
+class Chemical{
+    
+    var quantity: Int
+    
+    init(quantity: Int){
+        
+        self.quantity = quantity
+    }
+    
+}
