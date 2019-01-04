4af6025e175b86f170e79a2b8d8af9f171de5705
Parker Swanson <parker@liquidspace.com>
2019-01-04 10:49:14 -0600

    Add button with button: starting code

--- ChemiLog/FirstViewController.swift
+++ ChemiLog/FirstViewController.swift
@@ -16,8 +16,16 @@ class FirstViewController: UIViewController {
     }
 
     @IBAction func newChem(_ sender: Any) {
-        
+        var myButton: UIButton?
+        if !(myButton != nil){
+        let img = UIImage(named: "chemical-icon-png-6_25x25")
+        let myButton = UIButton(type: UIButton.ButtonType.custom)
+        myButton.frame = CGRect.init(x: 10, y: 10, width: 100, height: 45)
+        myButton.setImage(img, for: .normal)
+        myButton.addTarget(self, action: Selector(("btnTouched:")), for:.touchUpInside)
+        self.view.addSubview(myButton)
     }
     
+    }
 }
 
