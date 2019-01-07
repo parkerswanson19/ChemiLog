4cf32bed6caad5da9b60a676d500be0f2927a195
Maverick Hedlund <maverickhedlund@gmail.com>
2019-01-07 10:32:25 -0600

    Working button label

--- ChemiLog/FirstViewController.swift
+++ ChemiLog/FirstViewController.swift
@@ -18,9 +18,9 @@ class FirstViewController: UIViewController {
     @IBAction func newChem(_ sender: Any) {
         var myButton: UIButton?
         if !(myButton != nil){
-        let img = UIImage(named: "chemical-icon-png-6_25x25")
+        let img = UIImage(named: "flask")
         let myButton = UIButton(type: UIButton.ButtonType.custom)
-        myButton.frame = CGRect.init(x: 10, y: 10, width: 100, height: 45)
+        myButton.frame = CGRect.init(x: 15, y: 40, width: 100, height: 45)
         myButton.setImage(img, for: .normal)
         myButton.addTarget(self, action: Selector(("btnTouched:")), for:.touchUpInside)
         self.view.addSubview(myButton)
