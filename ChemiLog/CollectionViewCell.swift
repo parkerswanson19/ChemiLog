aa7acb906d2771ebb5ece01e60f5d903c2ae34f8
Maverick Hedlund <maverickhedlund@gmail.com>
2019-01-14 10:24:56 -0600

    Working cells

---
+++ ChemiLog/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  ChemiLog
+//
+//  Created by HEDLUND, MAVERICK on 1/9/19.
+//  Copyright © 2019 District196.org. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell{
+    
+    @IBOutlet weak var chemicalName: UILabel!
+}
--- ChemiLog/FirstViewController.swift
+++ ChemiLog/FirstViewController.swift
@@ -10,22 +10,26 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    let chemicalList = ["Oxygen","Carbon","Neon","Neon"]
+     @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func newChem(_ sender: Any) {
-        var myButton: UIButton?
-        if !(myButton != nil){
-        let img = UIImage(named: "flask")
-        let myButton = UIButton(type: UIButton.ButtonType.custom)
-        myButton.frame = CGRect.init(x: 15, y: 40, width: 100, height: 45)
-        myButton.setImage(img, for: .normal)
-        myButton.addTarget(self, action: Selector(("btnTouched:")), for:.touchUpInside)
-        self.view.addSubview(myButton)
-    }
     
     }
-}
 
+extension FirstViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return chemicalList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        cell.chemicalName.text = chemicalList[indexPath.row]
+        return cell
+    }
+}
