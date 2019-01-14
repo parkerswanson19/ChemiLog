bde3d76a4503e295c3bf26af35b70cd73df45ebb
Parker Swanson <parker@liquidspace.com>
2019-01-14 10:25:33 -0600

    deleted button functions

--- ChemiLog/FirstViewController.swift
+++ ChemiLog/FirstViewController.swift
@@ -15,33 +15,9 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    var buttons: [UIButton] = []
-    var xAxis = 10
-    let yAxis = 10
-    
     @IBAction func newChem(_ sender: Any) {
-        if (buttons.count == 0){
-        let img = UIImage(named: "chemical-icon-png-6_25x25")
-        let myButton = UIButton(type: UIButton.ButtonType.custom)
-        myButton.frame = CGRect.init(x: xAxis, y: yAxis, width: 100, height: 45)
-        myButton.setImage(img, for: .normal)
-        myButton.addTarget(self, action: Selector(("btnTouched:")), for:.touchUpInside)
-        self.view.addSubview(myButton)
-            buttons.append(myButton)
-        } else {
-            xAxis += 30
-                let img = UIImage(named: "chemical-icon-png-6_25x25")
-                let myButton = UIButton(type: UIButton.ButtonType.custom)
-            
-                myButton.frame = CGRect.init(x: xAxis, y: yAxis, width: 100, height: 45)
-                myButton.setImage(img, for: .normal)
-                myButton.addTarget(self, action: Selector(("btnTouched:")), for:.touchUpInside)
-                self.view.addSubview(myButton)
-            buttons.append(myButton)
-            
-        }
-    
+        
+        
     }
+
 }
-
-
