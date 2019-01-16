a87f05c859167def63bef300e5a53e0e3769834b
Maverick <maverick@ehs-s-xlib-108.local>
2019-01-16 10:57:34 -0600

    Popover

---
+++ ChemiLog/ManualAdd.swift
@@ -0,0 +1,14 @@
+//
+//  ManualAdd.swift
+//  ChemiLog
+//
+//  Created by Maverick on 1/16/19.
+//  Copyright © 2019 District196.org. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ManualAddController: UIViewController {
+    
+}
