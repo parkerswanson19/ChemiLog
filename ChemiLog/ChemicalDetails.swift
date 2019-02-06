bebe5ad1a679cc4bdd68de2c7cf28deb10e20dfa
Maverick <maverick@ehs-s-xlib-108.local>
2019-02-06 11:02:34 -0600

    work

--- ChemiLog/ChemicalDetails.swift
+++ ChemiLog/ChemicalDetails.swift
@@ -6,11 +6,38 @@
 //  Copyright © 2019 District196.org. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 class ChemicalDetailsController: UIViewController{
+    
+    
+    @IBOutlet weak var detailName: UILabel!
+    @IBOutlet weak var detailQuantity: UITextField!
+    @IBOutlet weak var detailCatalog: UITextField!
+    @IBOutlet weak var detailLastRefill: UITextField!
+    @IBOutlet weak var detailNextRefill: UITextField!
+    @IBOutlet weak var detailLabs: UITextField!
+    @IBOutlet weak var detailAmount: UITextField!
+    
+    
+    var detailName2 = ""
+    var detailQuantity2 = 1
+    var detailCatalog2 = 1
+    var detailLastRefill2 = ""
+    var detailNextRefill2 = ""
+    var detailLabs2 = " "
+    var detailAmount2 = 1
+    
     override func viewDidLoad() {
-        super.viewDidLoad()}
+        super.viewDidLoad()
+        print(detailName2)
+        detailName.text! = detailName2
+        detailQuantity.text! = String(detailQuantity2)
+        detailCatalog.text! = String(detailCatalog2)
+        detailLastRefill.text! = detailLastRefill2
+        detailNextRefill.text! = detailNextRefill2
+        detailLabs.text! = detailLabs2
+        detailAmount.text! = String(detailAmount2)
+    }
     
 }
--- ChemiLog/FirstViewController.swift
+++ ChemiLog/FirstViewController.swift
@@ -8,20 +8,21 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController  {
+    var myIndex = 0
     var chemicalList = [
         Chemical(quantity: 300, name: "Oxygen", catalogNumber: 45, lastRefill: "517", nextRefill: "617", usedLabs: "PennyLab", icon: "Flask", amount: 100),
         Chemical(quantity: 300, name: "Neon", catalogNumber: 45, lastRefill: "517", nextRefill: "617", usedLabs: "PennyLab", icon: "Flask", amount: 100)
     ]
-
-     @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     @IBAction func toAddChem(_ sender: Any) {
         performSegue(withIdentifier: "toManualAdd", sender: self)
     }
@@ -29,30 +30,24 @@ class FirstViewController: UIViewController {
     
     @IBAction func toSubmitNewChem(_ sender: UIStoryboardSegue)
     {
-            if let senderVC = sender.source as? ManualAddController{
-                senderVC.newChem.name = senderVC.nameIn.text ?? " "
-                senderVC.newChem.quantity = Int(senderVC.quantityIn.text ?? "0") ?? 0
-                senderVC.newChem.catalogNumber = Int(senderVC.catalogIn.text ?? "0") ?? 0
-                senderVC.newChem.lastRefill = senderVC.lastRefillIn.text ?? " "
-                senderVC.newChem.nextRefill = senderVC.nextUseIn.text ?? " "
-                senderVC.newChem.catalogLink = " "
-                senderVC.newChem.icon = "Flask"
-                senderVC.newChem.usedLabs = " "
-                senderVC.newChem.amount = 0
+        if let senderVC = sender.source as? ManualAddController{
+            senderVC.newChem.name = senderVC.nameIn.text ?? " "
+            senderVC.newChem.quantity = Int(senderVC.quantityIn.text ?? "0") ?? 0
+            senderVC.newChem.catalogNumber = Int(senderVC.catalogIn.text ?? "0") ?? 0
+            senderVC.newChem.lastRefill = senderVC.lastRefillIn.text ?? " "
+            senderVC.newChem.nextRefill = senderVC.nextUseIn.text ?? " "
+            senderVC.newChem.catalogLink = " "
+            senderVC.newChem.icon = "Flask"
+            senderVC.newChem.usedLabs = " "
+            senderVC.newChem.amount = 0
             chemicalList.append(senderVC.newChem)
         }
         collectionView.reloadData()
     }
-    @IBAction override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if (sender as? ChemicalDetailsController) != nil{
-            
-        }
-    }
-
     
 }
 
-extension FirstViewController: UICollectionViewDataSource{
+extension FirstViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return chemicalList.count
     }
@@ -60,8 +55,23 @@ extension FirstViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
         cell.chemicalName.text = chemicalList[indexPath.row].name
+        cell.tag = indexPath.row
         return cell
     }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let dvc = segue.destination as? ChemicalDetailsController{
+            if let cell = sender as? CollectionViewCell {
+                dvc.detailName2 = chemicalList[cell.tag].name
+                dvc.detailQuantity2 = chemicalList[cell.tag].quantity
+                dvc.detailCatalog2 = chemicalList[cell.tag].catalogNumber
+                dvc.detailLastRefill2 = chemicalList[cell.tag].lastRefill
+                dvc.detailNextRefill2 = chemicalList[cell.tag].nextRefill
+                dvc.detailLabs2 = chemicalList[cell.tag].usedLabs
+                dvc.detailAmount2 = chemicalList[cell.tag].amount
+            }
+        }
+    }
 }
+
+
