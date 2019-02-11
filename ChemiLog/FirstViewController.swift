e74871b1c6927ef67abfc3e6ed9f4cd897348071
Parker Swanson <parker@liquidspace.com>
2019-02-11 10:36:24 -0600

    commit so i can pull

--- ChemiLog/FirstViewController.swift
+++ ChemiLog/FirstViewController.swift
@@ -60,15 +60,11 @@ class FirstViewController: UIViewController {
         }
         collectionView.reloadData()
     }
-    @IBAction override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if (sender as? ChemicalDetailsController) != nil{
-            
-        }
         
     }
 
     
-}
+
 
 extension FirstViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
